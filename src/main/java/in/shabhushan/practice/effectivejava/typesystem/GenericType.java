a223db0a13d91a4c80b9520e7de0aa2b79f0d036
Shashi Bhushan <Shashi-Bhushan@users.noreply.github.com>
2019-10-08 03:37:45 +0530

    Generic Type modified

--- src/main/java/in/shabhushan/practice/effectivejava/typesystem/GenericType.java
+++ src/main/java/in/shabhushan/practice/effectivejava/typesystem/GenericType.java
@@ -3,6 +3,6 @@ package in.shabhushan.practice.effectivejava.typesystem;
 public class GenericType {
 
     public static <T extends Number> T pick(T t1, T t2) {
-        return t1 != null ? t1 : t2;
+        return t1 != null ? t1 : (T)new Integer(t2.hashCode());
     }
 }
